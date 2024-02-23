041117c7625c774a7abeb89d385e7444f938f1b4
Samer nasr <115714229+samer-nasr@users.noreply.github.com>
2024-02-23 21:05:49 +0200

    responsive

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -2,17 +2,30 @@ import './Sidebar.css';
 import Category from './Category/Category'
 import Price from './Price/Price'
 import Colors from './Colors/Colors'
+import { useState } from 'react';
 const Sidebar = ({handleChange}) =>{
+  // const [hide,setHide] = useState(false);
+  // const handleSidebar = () => { 
+  //   console.log("test");
+  //   setHide(current => !current);
+  // }
   return (
     <>
+    
       <section className='sidebar'>
         <div className='logo-container'>
-            <h1 className="shop-bag">🛒</h1>
+            <h1 className="shop-bag" >🛒</h1>
         </div>
 
-        <Category handleChange={handleChange}/>
-        <Price handleChange={handleChange}/>
-        <Colors handleChange={handleChange}/>
+        {/* <div className='sidebar-handle' 
+              style={{
+                      visibility : hide ? 'hidden' : ''}
+                    }
+        > */}
+          <Category handleChange={handleChange}/>
+          <Price handleChange={handleChange}/>
+          <Colors handleChange={handleChange}/>
+        {/* </div> */}
       </section>
     </>
   )
